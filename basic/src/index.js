38f209772fc62f5bd7fa8e2ac63edfef32f28a9e
fengyayun <941949529@qq.com>
2022-08-09 16:10:50 +0800

    feat:React合成事件的实现

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,6 +1,5 @@
 import React from './react';
 import ReactDOM from './react-dom'
-import { updateQueue } from './Component.js'
 class ClassComponent extends React.Component{
   constructor(props){
     super(props)
@@ -13,9 +12,10 @@ class ClassComponent extends React.Component{
    * 1.react控制的事件中setState的执行是异步且批量的
    * 2.不受react控制的事件执行是同步的且非批量的
   */
-  handleClick = (amount) =>{
+  handleClick = (event,amount) =>{
     console.log('handleClick');
-    updateQueue.isBatchingUpdate = true
+    console.log('event',event);
+    event.stopPropagation()
     // this.setState({num:this.state.num + amount})
     // this.setState((state) => ({ num: state.num + amount }), () => {
     //   console.log('callback', this.state);
@@ -35,12 +35,14 @@ class ClassComponent extends React.Component{
       this.setState({num:this.state.num + amount})
       console.log(this.state.num)
     }, 0);
-    updateQueue.batchUpdate()
+  }
+  divClick = () =>{
+    console.log('divClick');
   }
   render(){
-    return <div>
+    return <div onClick={this.divClick}>
      <h1>num:{this.state.num}</h1>
-     <button onClick={() => this.handleClick(10)}>+</button>
+     <button onClick={(e) => this.handleClick(e,10)}>+</button>
     </div>
   }
 }
--- basic/src/react-dom.js
+++ basic/src/react-dom.js
@@ -1,4 +1,5 @@
 import { REACT_TEXT } from './ReactSymbol'
+import { addEvent } from './event'
 function render(vdom,container){
   mount(vdom,container)
 }
@@ -56,8 +57,8 @@ function updateProps(dom,oldProps={},newProps={}){
         dom.style[style] = styleObj[style]
       }
     }else if (/^on[A-Z].*/.test(key)) {
-      dom[key.toLowerCase()] = newProps[key];
-      // addEvent(dom, key.toLowerCase(), newProps[key]);
+      // dom[key.toLowerCase()] = newProps[key];
+      addEvent(dom,key.toLowerCase(),newProps[key])
     }else {
       dom[key] = newProps[key]
     }
