c92212d42926efb549ed939d60c4cd959b45c514
fengyayun <941949529@qq.com>
2022-08-11 14:17:57 +0800

    feat:1.子组件的生命周期实现 2.简版的diff算法实现

---
+++ basic/history/08-childComponentLifeCycle/src/ReactSymbol.js
@@ -0,0 +1,3 @@
+export const REACT_ELEMENT = Symbol.for('Symbol(react.element)')
+export const REACT_TEXT = Symbol.for('Symbol(react.text)')
+export const REACT_FORWARD_REF = Symbol.for('Symbol(react.forward_ref)')
